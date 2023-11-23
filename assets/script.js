0c263f15f5ef66544a4937ad5da10e96c709e39a
Sivanishanthar Ram <r.nishanthar@gmail.com>
2023-11-23 18:29:29 -0500

    Skeleton of js and html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,52 @@
 // JavaScript Fundamentals Quiz
+
+//JavaScript questions multiple choice questions with answer
+var jsQuestions = [
+  {
+    question: "Which of the following is correct about JavaScript?",
+    choices: [
+      "A. Object-Based language",
+      "B. Assembly-language",
+      "C. Object-Oriented language",
+      "D. High-level language",
+    ],
+    answer: "A. Object-Based language",
+  },
+  {
+    question:
+      "Which of the following object is the main entery point to all client-side JavaScript features and APIs?",
+    choices: ["A. Position", "B. Window", "C. Standard", "D. Location"],
+    answer: "B. Window",
+  },
+  {
+    question: "Which of the following is not JavaScript data type?",
+    choices: [
+      "A. Null type",
+      "B. Undefined type",
+      "C. Number type",
+      "D. All of the mentioned",
+    ],
+    answer: "D. All of the mentioned",
+  },
+  {
+    question:
+      "Which of the following can be used to call a JavaScript Code Snippet?",
+    choices: [
+      "A. Function/Method",
+      "B. Preprocessor",
+      "C. Triggering Event",
+      "D. RMI",
+    ],
+    answer: "A. Function/Method",
+  },
+  {
+    question: "Why event handlers is needed in JavaScript?",
+    choices: [
+      "A. Adds inner HTML page to the code",
+      "B. Change the server location",
+      "C. Allows JavaScript code to alter the behaviours of windows",
+      "D. Performs handling of exceptions and occurrences",
+    ],
+    answer: "C. Allows JavaScript code to alter the behaviours of windows",
+  },
+];
