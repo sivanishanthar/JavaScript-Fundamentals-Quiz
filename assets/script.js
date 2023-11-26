63616bb1d8351bbb473060b6f034a45f6e47d484
Sivanishanthar Ram <r.nishanthar@gmail.com>
2023-11-26 18:31:12 -0500

    Initial set-ups of html, css, and js

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,11 @@
-// JavaScript Fundamentals Quiz
+var startQuiz = document.querySelector(".start-quiz");
+var quizDicription = document.querySelector(".quiz-dicription");
+var questionContainer = document.querySelector(".question-container");
 
-//JavaScript questions multiple choice questions with answer
-var jsQuestions = [
-  {
-    question: "Which of the following is correct about JavaScript?",
-    choices: [
-      "A. Object-Based language",
-      "B. Assembly-language",
-      "C. Object-Oriented language",
-      "D. High-level language",
-    ],
-    answer: "A. Object-Based language",
-  },
-  {
-    question:
-      "Which of the following object is the main entery point to all client-side JavaScript features and APIs?",
-    choices: ["A. Position", "B. Window", "C. Standard", "D. Location"],
-    answer: "B. Window",
-  },
-  {
-    question: "Which of the following is not JavaScript data type?",
-    choices: [
-      "A. Null type",
-      "B. Undefined type",
-      "C. Number type",
-      "D. All of the mentioned",
-    ],
-    answer: "D. All of the mentioned",
-  },
-  {
-    question:
-      "Which of the following can be used to call a JavaScript Code Snippet?",
-    choices: [
-      "A. Function/Method",
-      "B. Preprocessor",
-      "C. Triggering Event",
-      "D. RMI",
-    ],
-    answer: "A. Function/Method",
-  },
-  {
-    question: "Why event handlers is needed in JavaScript?",
-    choices: [
-      "A. Adds inner HTML page to the code",
-      "B. Change the server location",
-      "C. Allows JavaScript code to alter the behaviours of windows",
-      "D. Performs handling of exceptions and occurrences",
-    ],
-    answer: "C. Allows JavaScript code to alter the behaviours of windows",
-  },
-];
+var question = document.querySelector("#question");
+var answers = document.querySelector("#answers");
+
+startQuiz.addEventListener("click", function () {
+  quizDicription.style.display = "none";
+  questionContainer.style.display = "block";
+});
